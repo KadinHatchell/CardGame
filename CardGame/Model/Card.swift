b400a584ea5670e23cd8b1b10733e44a9110011d
Kadin Hatchell <kadinahatchell@gmail.com>
2016-10-25 09:23:04 -0600

    Created Project

---
+++ CardGame/Model/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  CardGame
+//
+//  Created by Hatchell, Kadin on 10/25/16.
+//  Copyright © 2016 Hatchell, Kadin. All rights reserved.
+//
+
+import Foundation
---
+++ CardGame/Model/PlayingCard.swift
@@ -0,0 +1,9 @@
+//
+//  PlayingCard.swift
+//  CardGame
+//
+//  Created by Hatchell, Kadin on 10/25/16.
+//  Copyright © 2016 Hatchell, Kadin. All rights reserved.
+//
+
+import Foundation
