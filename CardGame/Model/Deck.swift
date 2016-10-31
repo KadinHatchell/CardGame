a60af2e35063813c0f62cd9ad9a2af140b1ed95c
Kadin Hatchell <kadinahatchell@gmail.com>
2016-10-31 08:21:54 -0600

    added deck and playing card deck

---
+++ CardGame/Model/Deck.swift
@@ -0,0 +1,14 @@
+//
+//  Deck.swift
+//  CardGame
+//
+//  Created by Hatchell, Kadin on 10/31/16.
+//  Copyright © 2016 Hatchell, Kadin. All rights reserved.
+//
+
+import UIKit;
+
+class Deck : PlayingCard
+{
+    
+}
---
+++ CardGame/Model/PlayingCardDeck.swift
@@ -0,0 +1,9 @@
+//
+//  PlayingCardDeck.swift
+//  CardGame
+//
+//  Created by Hatchell, Kadin on 10/31/16.
+//  Copyright © 2016 Hatchell, Kadin. All rights reserved.
+//
+
+import Foundation
